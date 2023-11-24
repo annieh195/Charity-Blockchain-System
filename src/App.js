4dd6977beef331dcb51f8946e98d5f09f4dc4a2e
akame <113761684+TTAkame@users.noreply.github.com>
2023-11-23 22:31:08 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DonationPage from './pages/js/DonationPage';
 import CreatePage from './pages/js/CreatePage';
+import Home from './pages/js/Home'; 
 import NavBar from './pages/js/NavBar';
 
 function App() {
@@ -9,8 +10,9 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<DonationPage />} />
+        <Route path="/" element={<Home />} /> 
         <Route path="/create" element={<CreatePage />} />
+        <Route path="/donation/:charityName" element={<DonationPage />} /> {/* Dynamic route */}
       </Routes>
     </Router>
   );
