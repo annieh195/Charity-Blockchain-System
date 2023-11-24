5645d2d850e8e007cc8fa3b28c3e52e2c3594d0c
akame <113761684+TTAkame@users.noreply.github.com>
2023-11-23 22:52:10 -0800

    Update Web3Client.js

--- src/pages/js/Web3Client.js
+++ src/pages/js/Web3Client.js
@@ -1,30 +1,42 @@
-
 import Web3 from 'web3';
+
 let web3;
 
 const initWeb3 = async () => {
-  if (typeof window.ethereum !== 'undefined') {
+  // Check for Ethereum provider
+  if (window.ethereum) {
     web3 = new Web3(window.ethereum);
     try {
-      await window.ethereum.enable();
+      // Request account access if needed
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
     } catch (error) {
-      console.error("User denied account access");
+      console.error("User denied account access", error);
     }
   } 
-  else if (typeof window.web3 !== 'undefined') {
+  // Legacy dapp browsers...
+  else if (window.web3) {
     web3 = new Web3(window.web3.currentProvider);
   } 
+  // If no injected web3 instance is detected, fall back to Infura
   else {
-    console.error("No web3 provider detected");
+    console.error("No web3 provider detected, falling back to Infura");
+    web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY'));
   }
 };
 
 // Create a new fund
-const createNewFund = async (charityName, description, fundsRequired) => {
+const createNewFund = async (charityName, description, fundsRequired, contractABI, contractAddress) => {
   try {
-    const accounts = await web3.eth.getAccounts();
-    const contract = new web3.eth.Contract(/* Your contract ABI here */ /* Your contract address here */);
+    if (!web3) {
+      throw new Error("Web3 is not initialized");
+    }
 
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length === 0) {
+      throw new Error("No account is available");
+    }
+
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
     await contract.methods.createFund(charityName, description, fundsRequired).send({
       from: accounts[0],
     });
